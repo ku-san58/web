7e495c4cafb09ee82673ed5437c7de071ea7e0f3
yassan <yassan@yassan_PC>
2020-02-13 01:27:30 +0900

    komit

---
+++ WebContent/Mypage.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>マイページ</title>
+</head>
+<body>
+	<form action="MypageServlet" method="get">
+	<input type="submit" value="新規投稿">
+	</form>
+</body>
+</html>
--- src/pac1/MypageServlet.java
+++ src/pac1/MypageServlet.java
@@ -23,20 +23,20 @@ public class MypageServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet rs = null;
 		ArrayList<String> list = new ArrayList<String>();
-		
+
 		try {
-			Class.forName("org.mariadb.jdbc.Driver");
+			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(
-				"jdbc:mysql://10.15.121.37:3306/webapp2019_sgt2?serverTimezone=JST",
-				"user_sgt2", "sgt2");
+					"jdbc:mysql://localhost:3306/web?serverTimezone=JST",
+				"root", "yassan5800");
 			stmt = conn.createStatement();
 			rs = stmt.executeQuery("SELECT * FROM blog");
-			
+
 			while (rs.next()) {
 				String s = rs.getString("day")+ ":" + rs.getString("title");
 				list.add(s);
@@ -63,21 +63,21 @@ public class MypageServlet extends HttpServlet {
 				}
 			}
 		}
-		
+
 		PrintWriter out = response.getWriter();
 		out.println("<!DOCTYPE html>");
 		out.println("<html>");
 		out.println("<head>");
 		out.println("<meta charset=\"UTF-8\">");
-		out.println("<title>練習６－１</title>");
+		out.println("<title>MypageServlet</title>");
 		out.println("</head>");
 		out.println("<body>");
-	
-		
+
+
 		for (String str : list) {
 			out.println(str + "<br>");
 		}
-		
+
 		out.println("</body>");
 		out.println("</html>");
 	}
